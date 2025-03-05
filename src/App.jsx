56f185d9caf181758b70b28f663a0ff9225ffc71
nopalss <holanakdo@gmail.com>
2025-03-05 09:20:28 +0700

    inital react

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold">Hello world </h1>
+      <button className="btn btn-success">Success</button>
+    </>
+  );
+}
+
+export default App;
